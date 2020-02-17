6513c7eaa212ff953f3a4dfc8a1f0b368b948387
wdjpng <muenzel.lukas@gmail.com>
2020-02-17 12:00:00 +0100

    Added task toposort22

---
+++ Camp/Graph Lecture/Toposort22/cmake-build-debug/CMakeFiles/Toposort22.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Toposort22.dir/main.cpp.o"
+  "Toposort22"
+  "Toposort22.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Toposort22.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Camp/Graph Lecture/Toposort22/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+#define int long long
+
+using namespace std;
+//Scoring to points currently
+void bfs(int start, string& output, vector<vector<int>>& E, vector<bool>& visited, vector<bool>& isInCurrentSearch){
+    visited[start] = true;
+    isInCurrentSearch[start] = true;
+
+    for (int i = 0; i < E[start].size(); ++i) {
+        int nextNode = E[start][i];
+
+        if(isInCurrentSearch[nextNode]){
+            cout << "IMPOSSIBLE\n";
+            exit(0);
+        }
+
+        if(!visited[nextNode]){
+
+            bfs(nextNode, output, E, visited, isInCurrentSearch);
+        }
+    }
+
+    output+= to_string(start) + ' ';
+    isInCurrentSearch[start] = false;
+}
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> E(n);
+
+    vector<bool> visited(n, false);
+    vector<bool> isInCurrentSearch(n, false);
+
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        E[a].push_back(b);
+    }
+
+    string output = "";
+    for (int start = 0; start < n; ++start) {
+        if(!visited[start]){
+            if(!visited[start]){
+                bfs(start, output, E, visited, isInCurrentSearch);
+            }
+        }
+    }
+
+    cout << "POSSIBLE\n" << output << "\n";
+}
