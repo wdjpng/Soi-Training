091eba1a746a44cdf00b622e862a62b84d9a5e55
wdjpng <muenzel.lukas@gmail.com>
2020-03-15 12:00:00 +0100

    Added 2p task tollroads

---
+++ 2P 2020/tollroads/cmake-build-debug/CMakeFiles/tollroads.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tollroads.dir/main.cpp.o"
+  "tollroads"
+  "tollroads.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tollroads.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
