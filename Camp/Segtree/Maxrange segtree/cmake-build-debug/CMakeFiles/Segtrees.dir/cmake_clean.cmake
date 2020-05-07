7ae7a604c71110b5eca4a20704829cbdf68245c5
wdjpng <muenzel.lukas@gmail.com>
2020-05-07 12:00:00 +0200

    Added maxrange segtree

---
+++ Camp/Segtree/Maxrange segtree/cmake-build-debug/CMakeFiles/Segtrees.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Segtrees.dir/main.cpp.o"
+  "Segtrees"
+  "Segtrees.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Segtrees.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
