95fb0175b6cb16e225c7c9aacaaf65d96199ba3d
wdjpng <muenzel.lukas@gmail.com>
2020-06-29 12:00:00 +0200

    Added task cutvertivesandbridges

---
+++ Camp/Graph Lecture/Cutverticesandbridges/cmake-build-debug/CMakeFiles/Cutverticesandbridges.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cutverticesandbridges.dir/main.cpp.o"
+  "Cutverticesandbridges"
+  "Cutverticesandbridges.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cutverticesandbridges.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Camp/Graph Lecture/Cutverticesandbridges/cmake-build-debug/CMakeFiles/Cutverticesandbridges.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Cutverticesandbridges.
+# This may be replaced when dependencies are built.
